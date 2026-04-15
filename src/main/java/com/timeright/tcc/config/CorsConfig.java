6ba2ee1b10579c4baf16c1e21e4851082447b271
isxblly07 <rm94877@estudante.fieb.edu.br>
2026-04-15 10:47:59 -0300

    funcionando

--- src/main/java/com/timeright/tcc/config/CorsConfig.java
+++ src/main/java/com/timeright/tcc/config/CorsConfig.java
@@ -1,17 +1,16 @@
 package com.timeright.tcc.config;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.cors.CorsConfigurationSource;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.web.SecurityFilterChain;
-
 import java.util.List;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@SuppressWarnings("unused")
 @Configuration
 public class CorsConfig {
 
@@ -30,6 +29,7 @@ public class CorsConfig {
         return source;
     }
 
+    @SuppressWarnings("removal")
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
