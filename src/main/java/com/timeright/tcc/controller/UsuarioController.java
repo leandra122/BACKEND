055d6b9daf5ee4bc303a727312e5975e7bc98754
leandra122 <rm94446@estudante.fieb.edu.br>
2026-05-29 08:38:23 -0300

    login e cadastro do usuario e adm

--- src/main/java/com/timeright/tcc/controller/UsuarioController.java
+++ src/main/java/com/timeright/tcc/controller/UsuarioController.java
@@ -1,16 +1,24 @@
 package com.timeright.tcc.controller;
 
-import com.timeright.tcc.model.dto.LoginRequest;
-import com.timeright.tcc.model.entity.Usuario;
-import com.timeright.tcc.services.UsuarioService;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 import java.util.Map;
 
-@CrossOrigin(origins = "*")
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.timeright.tcc.model.dto.LoginRequest;
+import com.timeright.tcc.model.entity.Usuario;
+import com.timeright.tcc.services.UsuarioService;
+
 @RestController
 @RequestMapping("/usuarios")
 public class UsuarioController {
